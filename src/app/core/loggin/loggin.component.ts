42f0a83d0fd34ecad165d55c6b0c6616df2487af
Luis Banegas <55450718+Shadow504@users.noreply.github.com>
2021-07-14 23:16:09 -0600

    Navegacion vista principal y Validacion de prueba.
    Implementacion de la validacion de un usuario definido y de ser valido, redireccionar a la vista principal Admin.

--- src/app/core/loggin/loggin.component.ts
+++ src/app/core/loggin/loggin.component.ts
@@ -1,34 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
-
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-loggin',
   templateUrl: './loggin.component.html',
   styleUrls: ['./loggin.component.sass']
 })
 export class LogginComponent implements OnInit {
+  //variable para controlar el acceso a rutas.
+  stateSesion:boolean = false;
 
   user:any = {
     email: '',
     password: ''
   }
 
-  constructor( private httpCliente:HttpClient ) {}
+  userValidate = {
+    email: 'admin@gmail.com',
+    password: 'root'
+  }
+  constructor( private httpCliente:HttpClient, private route:Router, private activatedRoute:ActivatedRoute ) {}
 
   ngOnInit(): void {
   }
   // ,{email:this.user.email, password:this.user.password}
 
   login(){
-    this.httpCliente.post('http://localhost:3000/login',{email:this.user.email, password:this.user.password}).subscribe(res =>{
+    /* this.httpCliente.post('http://localhost:3000/login',{email:this.user.email, password:this.user.password}).subscribe(res =>{
       console.log(res);
     }); 
-    // console.log("datos enviados");
-    // console.log(this.user);
-    
-    console.log(this.user.email,this.user.password);
-    
+    console.log("datos enviados");
+    console.log(this.user); */
+    if(this.user.email == this.userValidate.email && this.userValidate.password == this.user.password){
+      this.route.navigate(['/admin']);
+      console.log(this.user.email,this.user.password);
+      this.stateSesion = true;
+    }else{
+      this.route.navigate(['/']);
+      this.user.email = "";
+      this.user.password = "";
+    }
   }
 
 }
